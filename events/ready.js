1b2bb861a6846b547af510aa9e8bd9f0e9678989
Glitch (aggsy) <none>
2021-01-04 22:44:53 +0000

    👸🍐 Updated with Glitch

---
+++ events/ready.js
@@ -0,0 +1,26 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+  
+module.exports = client => {
+var oyun = [
+         " 🔥 -yenilikler Yeni Gelen Komutlar",
+         " 🔥 -yardım V12 -davet",
+     
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "https://www.twitch.tv/");
+        }, 2 * 20000);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] -----Developer By ɱႦ DumPS †#0002------`);
+  client.user.setStatus("idle");
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+};
